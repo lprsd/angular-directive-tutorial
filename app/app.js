bc374c31206e59290f68c664b58e1e1d46f50971
Lakshman Prasad <lakshman@lakshmanprasad.com>
2013-12-04 14:50:59 +0530

    Directives Stage3: Multiple directives on a single element.

--- app/app.js
+++ app/app.js
@@ -3,9 +3,16 @@ var app = angular.module('demo', [])
 app.directive('chart', function (){
     return {
         restrict: 'A',
-        template: "<div>charts go here</div>",
         link: function(){
             alert("Hey! I'm from the directive");
+        }
+    }
+})
+app.directive('chart2', function (){
+    return {
+        restrict: 'A',
+        link: function(){
+            alert("Other chart directive2");
         }
     }
 })
