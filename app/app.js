5cc2a9ccf5f575b702f5850574db953692f84114
Lakshman Prasad <lakshman@lakshmanprasad.com>
2013-12-07 12:02:55 +0530

    demo changes

--- app/app.js
+++ app/app.js
@@ -6,3 +6,69 @@ app.directive('chart', function (){
         template: "<div>charts go here</div>"
     }
 })
+
+app.directive('mydirective', function(){
+    return {
+        restrict:'A',
+        template: "My first directive",
+        link: function(scope, element, attrs){
+            element.bind('mouseenter',function(){
+                alert("I'm working!");
+            });
+        },
+    }
+})
+
+app.directive('highlighton', function(){
+    return {
+        link:    function(scope, element, atts)
+        {
+            element.bind('mouseenter',function(){
+                element.addClass('jumbotron');
+            });
+        }
+//        template:"Highlight On"
+    }
+})
+
+app.directive('highlightoff', function(){
+    return {
+        link:    function(scope, element, atts)
+        {
+            element.bind('mouseleave',function(){
+                element.removeClass('jumbotron');
+            });
+        },
+        template:"<div>Highlight Off    <div ng-transclude></div></div>",
+        transclude: true
+    }
+})
+
+app.directive('formatting', function(){
+    return {
+        scope: {
+            addbreak: function(element, attrs){
+
+            },
+
+            makeitalic: function(elemnt, attrs){
+
+            }
+        }
+    }
+})
+
+app.directive('italicize', function(){
+    return{
+        required: 'formatting',
+        link: function(scope, element, attrs){
+
+        }
+    }
+})
+
+app.directive('model', function(){
+    return {
+
+    }
+})
